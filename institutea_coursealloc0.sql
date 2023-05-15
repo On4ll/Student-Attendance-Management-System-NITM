c3962e2362bb4f49d8cd61792415c6bf88f3a778
Anal Probaho Roy <47379363+On4ll@users.noreply.github.com>
2023-05-16 02:15:11 +0600

    Updated Database

---
+++ institutea_coursealloc0.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: institutea
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `coursealloc`
+--
+
+DROP TABLE IF EXISTS `coursealloc`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `coursealloc` (
+  `Cid` varchar(127) NOT NULL,
+  `Cname` varchar(127) DEFAULT NULL,
+  `Fac1` varchar(127) DEFAULT NULL,
+  `Fac2` varchar(127) DEFAULT NULL,
+  `Fac3` varchar(127) DEFAULT NULL,
+  PRIMARY KEY (`Cid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `coursealloc`
+--
+
+LOCK TABLES `coursealloc` WRITE;
+/*!40000 ALTER TABLE `coursealloc` DISABLE KEYS */;
+INSERT INTO `coursealloc` VALUES ('C1','Physical Studies','F1','F2','F3'),('C2','Spiritual Studies','F2',NULL,'F4'),('C3','Dota 2',NULL,'F3',NULL),('C4','Food','F3',NULL,'F1'),('C5','Swimming',NULL,'F2',NULL),('CS302','Software Engineering',NULL,NULL,'F4');
+/*!40000 ALTER TABLE `coursealloc` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-16  1:44:03
